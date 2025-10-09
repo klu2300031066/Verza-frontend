00ac8a3861da264b61944503134609e9a7d9711f
mohith D <163638586+klu2300031066@users.noreply.github.com>
2025-10-09 10:06:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import { useState } from 'react'
 import './App.css'
-import Login from './components/Login'
-import Signup from './components/Signup'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import HomePage from './components/HomePage'
-import AboutUs from './components/Aboutus'
-import Computers from './components/Computers'
-import Mobiles from './components/Mobiles'
-import Laptops from './components/Laptops'
-import Pendrives from './components/Pendrives'
+import Login from './components/Login';
+import Signup from './components/Signup';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import AboutUs from './components/Aboutus';
+import Computers from './components/Computers';
+import Mobiles from './components/Mobiles';
+import Laptops from './components/Laptops';
+import Pendrives from './components/Pendrives';
 import Home from './components/Home'
 import ProductPage from './components/ProductPage'
-import Cart from './components/Cart'
-import Payment from './components/Payment'
-import Orders from './components/Orders'
+import Cart from './components/Cart';
+import Payment from './components/Payment';
+import Orders from './components/Orders';
+
+
 
 function App() {
   const [count, setCount] = useState(0)
-
+  
   return (
-    <Router basename="/ecommerce">
+    <>
+    <Router>
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
@@ -38,6 +41,7 @@ function App() {
         </Route>
       </Routes>
     </Router>
+    </>
   )
 }
 
