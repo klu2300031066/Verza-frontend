66cceaee21580a6268df6db284f85cf3fd570993
mohith D <163638586+klu2300031066@users.noreply.github.com>
2025-10-09 10:11:16 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://54.162.203.83:8081/api/products";  // Backend API URL
+const API_BASE_URL = "http://54.224.13.73:8081/api/products";  // Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
@@ -15,5 +15,5 @@ export const getProducts = async (category = "") => {
 
 // Get product image URL
 export const getProductImageUrl = (imagePath) => {
-  return `http://54.226.80.213:8081/api/products/images/${imagePath}`;
+  return `http://54.224.13.73:8081/api/products/images/${imagePath}`;
 };
