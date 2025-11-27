4048b86d486ce1721f30978a02dec2ba00d618b5
mohith D <163638586+klu2300031066@users.noreply.github.com>
2025-11-27 19:28:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,32 +15,30 @@ import Cart from './components/Cart';
 import Payment from './components/Payment';
 import Orders from './components/Orders';
 
-
-
 function App() {
   const [count, setCount] = useState(0)
   
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />}>
-          <Route index element={<AboutUs />} />
-          <Route path="home" element={<Home />} />
-          <Route path="about-us" element={<AboutUs />} />
-          <Route path="computers" element={<Computers />} />
-          <Route path="mobiles" element={<Mobiles />} />
-          <Route path="laptops" element={<Laptops />} />
-          <Route path="pendrives" element={<Pendrives />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="productpage" element={<ProductPage />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="payment" element={<Payment />} />
-          <Route path="orders" element={<Orders />} />
-        </Route>
-      </Routes>
-    </Router>
+      <Router basename="/ecommerce">  {/* <-- add this */}
+        <Routes>
+          <Route path="/" element={<HomePage />}>
+            <Route index element={<AboutUs />} />
+            <Route path="home" element={<Home />} />
+            <Route path="about-us" element={<AboutUs />} />
+            <Route path="computers" element={<Computers />} />
+            <Route path="mobiles" element={<Mobiles />} />
+            <Route path="laptops" element={<Laptops />} />
+            <Route path="pendrives" element={<Pendrives />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="productpage" element={<ProductPage />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="payment" element={<Payment />} />
+            <Route path="orders" element={<Orders />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   )
 }
